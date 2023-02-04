6ec4d99c987e80540e7cea3a1423e3b9d3895b7e
Debasish Mohapatra <mohapatradebasish92@gmail.com>
2023-02-05 00:32:19 +0100

    Changed the worker entrypoint file to run celery workers in start

--- worker/scripts/entrypoint.sh
+++ worker/scripts/entrypoint.sh
@@ -2,10 +2,10 @@
 
 echo "== Starting Django app: $INSTALL_DIR: $DJANGO_SETTINGS_MODULE"
 echo "Waiting for postgres..."
-while ! nc -z ${MY_DB_HOST:-wiki_store_db} ${MY_DB_PORT:-5432}; do
+while ! nc -z ${MY_DB_HOST:-web_server_db} ${MY_DB_PORT:-5432}; do
   sleep 0.1
 done
 echo "DB Ready ..."
 
 echo "Starting worker..."
-python src/worker.py
+celery -A celery_conn:app worker -l INFO
